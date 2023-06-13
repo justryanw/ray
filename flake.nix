e11a678b6859af9c751576fe1e8059138eb2291a
Ryan <ryanjwalker2001@gmail.com>
2023-06-13 21:21:21 +0100

    flake lock

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
+      overlays = [
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default;
+        })
+      ];
+
       allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit overlays system; };
       });
     in
     {
